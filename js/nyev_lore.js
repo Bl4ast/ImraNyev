2d449addcbc7370f4015d51e77f1f42a8495817f
Murilo Pereira <134763300+Bl4ast@users.noreply.github.com>
2026-07-16 11:32:30 -0300

    Clean up lore text in nyev_lore.js
    Removed placeholder text from lore sections.

--- js/nyev_lore.js
+++ js/nyev_lore.js
@@ -1,10 +1,10 @@
 const nyev_cap1_texto = `
 <div class="lore-subchapter">
     <h3>1.1 O início</h3>
-    <p>Os cosmos, perfeitos e imperfeitos... (sua lore aqui)</p>
+    <p>p>
 </div>
 `;
 
 const nyev_cap2_texto = `
-<p>Escreva os segredos da menina rosa aqui.</p>
+<p></p>
 `;
