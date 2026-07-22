5eff2f0fbcb00cdb9c3be39b29abb5bc9dd17f5a
Murilo Pereira <134763300+Bl4ast@users.noreply.github.com>
2026-07-22 12:27:40 -0300

    Update nyev_extras.js

--- js/nyev_extras.js
+++ js/nyev_extras.js
@@ -4,6 +4,17 @@ const nyev_curiosidades = [
 ];
 
 const nyev_qa = [
-    { q: 'TESTE TESTE ', a: 'TESTE TESTE' },
-    { q: 'TESTE TESTE ', a: 'TESTE TESTE' }
+    { q: 'Acredita em finais felizes?', a: 'Acredito que toda alma merece um final que lhe traga paz no espírito.' },
+    { q: 'Acredita em amor à primeira vista?', a: 'Claro, fomos feitos para amar o outro, especialmente aqueles com que temos uma conexão única.' },
+    { q: 'O que a faz se sentir envergonhada?', a: 'Não sei ao certo... Acho que fico meio tímida quando me elogiam em voz alta, parece meio irreal alguém querendo fazer isso por vontade própria, também não me sinto tão confortável quando preciso falar perto de muita gente, é estranha a sensação de tantos olhares apontados em minha direção.' },
+    { q: 'Qual a coisa mais bela que já viu?', a: '-' },
+    { q: 'O que considera bonito nos outros?', a: 'Sinceridade, gosto quando os outros a minha volta são sinceros com seus próprios sentimentos, eu queria conseguir ser como eles, faz eu me sentir inspirada para tentar alcançar o nível deles.' },
+    { q: 'Você cumpre suas promessas?', a: 'Eu sempre dou meu máximo para cumprir elas, mas às vezes falo segredos que deveriam ficar guardados apenas no meu coração... Mas nunca de propósito!' },
+    { q: 'Qual seu maior medo?', a: 'É difícil apontar somente um, tenho medo de várias coisas para ser sincera, ficar sozinha é um deles, me sinto abandonada, não sei se deveria falar os outros para ser sincera, tenho medo de ser julgada...' },
+    { q: 'Quem é Nyev quando não há o outro para cuidar?', a: 'Acho que apenas uma pessoa solitária, presa em seu canto enquanto se perde nos pensamentos.' },
+    { q: 'Se tirassem sua empatia de você, o que restaria?', a: 'Majoritriamente coisas pessimistas, gosto de pensar que as pessoas que trazem minhas melhores características. Mas acho que mesmo sem ligar para elas, eu ainda seria gentil, eu espero.' },
+    { q: 'Qual seria sua maior derrota?', a: 'Perder alguém por conta de minhas próprias ações, espero que meu impacto na vida dos outros seja apenas positivo.' },
+    { q: 'Qual é a mentira que você conta para continuar seguindo em frente?', a: 'Que um dia tudo irá valer a pena. Me preocupo que o futuro que me aguarda faça com que tudo que vivi seja inútil, e às vezes as preocupações viram grandes espirais de dúvidas existenciais.' },
+    { q: 'Se proteger alguém significasse se tornar um monstro, você aceitaria?', a: 'Não sei, não gosto de me imaginar na pele de um monstro, talvez se fosse um monstro justificado, dependeria muito da situação também... Acho que só pelo Imra.' },
+    { q: 'Como seria seu final feliz?', a: 'Poder descansar em algum lugar calmo e silêncio, somente eu e o Imra, olhando para a paisagem com o coração preenchido.' },
 ];
